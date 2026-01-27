de624008746d4b5271e2dd9d92fe9f20bdf51f39
BaoKhanh781 <152123666+BaoKhanh781@users.noreply.github.com>
2026-01-27 08:09:06 -0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2017 John Edgar Park for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+# Circuit Playground NeoPixel
+import time
+import board
+
+def calculate_sum(a, b):
+    total = a + b
+    return total
+
+x = 5
+y = 10
+z = calculate_sum(x, y)
+print(z)
